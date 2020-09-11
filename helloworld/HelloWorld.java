d3873dffb6a36575c1a33625316bc4cab7a8460b
二一王一二 <sw3310@columbia.edu>
2020-09-11 20:42:29 +0800

    add Auther

--- helloworld/HelloWorld.java
+++ helloworld/HelloWorld.java
@@ -1,7 +1,10 @@
 package helloworld;
 
 public class HelloWorld {
-    //主函数
+    /*主函数
+    @Auther Wang
+    自行车在
+    */
     public static void main(String[] args) {
         System.out.println("hello");
     }
