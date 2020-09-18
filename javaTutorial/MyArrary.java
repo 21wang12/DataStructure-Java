fc1ef47f803f16dec36f6ea7bd3483e6d0e8a76e
二一王一二 <sw3310@columbia.edu>
2020-09-18 18:14:50 +0800

    delete empty line

--- javaTutorial/MyArrary.java
+++ javaTutorial/MyArrary.java
@@ -5,7 +5,7 @@ public class MyArrary{
         /**
          * 一维数组
          */
-        //声明y
+        //声明
         int[] intArrary;
         //创建
         intArrary = new int[10];
@@ -13,7 +13,6 @@ public class MyArrary{
         intArrary[0] = 1;
         //访问
         System.out.println("intArray[0]=:"+intArrary[0]);
-
         //一维数组的简化的声明、创建和初始化
         int[] intArrary01 = {
             10,11,12,
@@ -32,7 +31,6 @@ public class MyArrary{
         multiArrary[0][3] = 3;
         //访问
         System.out.println("multiArrary[0][3]=:"+multiArrary[0][3]);
-
         //多维数组的简化声明、创建和初始化
         int[][] multiArrary01={
             intArrary,
