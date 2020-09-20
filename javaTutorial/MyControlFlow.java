ae941723eec775071dfb9a94f911257409f132f8
二一王一二 <sw3310@columbia.edu>
2020-09-20 17:05:06 +0800

    my control flow

---
+++ javaTutorial/MyControlFlow.java
@@ -0,0 +1,13 @@
+package javaTutorial;
+
+public class MyControlFlow {
+
+    public static void main(String[] args) {
+        String s = "2";
+        switch(s){
+            case "1":
+                System.out.println("1");
+                break;
+        }
+    }
+}
