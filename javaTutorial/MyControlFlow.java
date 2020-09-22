f0bb6940786c61c8a51b7682572d53851198aa3a
二一王一二 <sw3310@columbia.edu>
2020-09-22 18:21:57 +0800

    add switch control flow

--- javaTutorial/MyControlFlow.java
+++ javaTutorial/MyControlFlow.java
@@ -1,14 +1,7 @@
 package javaTutorial;
 
 public class MyControlFlow {
-
     public static void main(String[] args) {
-        String s = "1";
-        switch(s){
-            case "1":
-                System.out.println("1");
-            default :
-                System.out.println("defaul");
-        }
+        System.out.println("I O U");
     }
 }
