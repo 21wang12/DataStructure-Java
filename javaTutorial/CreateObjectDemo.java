1c5c8dce15618f957a433c200fdd7a9eaf56413e
二一王一二 <sw3310@columbia.edu>
2020-10-12 08:41:26 +0800

    add gutnore

--- javaTutorial/CreateObjectDemo.java
+++ javaTutorial/CreateObjectDemo.java
@@ -2,28 +2,18 @@ package javaTutorial;
 
 public class CreateObjectDemo {
 
-    public static void main(String[] args) {
-		
-        // Declare and create a point object and two rectangle objects.
-        Point originOne = new Point(23, 94);
-        Rectangle rectOne = new Rectangle(originOne, 100, 200);
-        Rectangle rectTwo = new Rectangle(50, 100);
-		
-        // display rectOne's width, height, and area
-        System.out.println("Width of rectOne: " + rectOne.width);
-        System.out.println("Height of rectOne: " + rectOne.height);
-        System.out.println("Area of rectOne: " + rectOne.getArea());
-	
-        // set rectTwo's position
-        rectTwo.origin = originOne;
-		
-        // display rectTwo's position
-        System.out.println("X Position of rectTwo: " + rectTwo.origin.x);
-        System.out.println("Y Position of rectTwo: " + rectTwo.origin.y);
-		
-        // move rectTwo and display its new position
-        rectTwo.move(40, 72);
-        System.out.println("X Position of rectTwo: " + rectTwo.origin.x);
-        System.out.println("Y Position of rectTwo: " + rectTwo.origin.y);
+    int varInt = setValueFun();
+
+    int setValueFun(){
+        return varInt;
     }
+
+    public static void main(String[] args) {
+        System.out.println(new CreateObjectDemoSub().varInt);
+    }
+}
+
+class CreateObjectDemoSub extends CreateObjectDemo{
+
+
 }
