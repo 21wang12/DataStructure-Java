c810c26edc0e9dc98bcce8368944a4f9e39ef0b0
二一王一二 <sw3310@columbia.edu>
2020-10-27 10:43:01 +0800

    LeetCode-144. 二叉树的前序遍历

---
+++ LeetCode/src/Solution.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.Stack;
+/**
+ * Commit Message Example:
+ * LeetCode-xxx.xxxx
+ */
+
+
+
+class Solution {
+    
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<TreeNode> l = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode currentNode = root;
+        while(currentNode != null || ! stack.empty()){
+            if(currentNode != null){
+                l.add(currentNode);
+                stack.push(currentNode);
+                currentNode = currentNode.left;
+            }else{
+                currentNode = stack.pop().right;
+            }
+
+        }
+        return l;
+    }
+
+    public static void main(String[] args) {
+        
+    }
+
+}
