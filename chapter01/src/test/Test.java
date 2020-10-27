75272b1d96a32cec83eb64d4ab169160d5432339
二一王一二 <sw3310@columbia.edu>
2020-10-27 10:47:04 +0800

    aa

--- chapter01/src/test/Test.java
+++ chapter01/src/test/Test.java
@@ -1,8 +1,17 @@
 package test;
 
+import java.util.Calendar;
+import java.util.Date;
+
 import encap_1.Student;
 
 public class Test {
     Student s1 = new Student();
+    public static void main(String[] args) {
+        StringBuffer sb= new StringBuffer("hellp");
+        Date d = new Date();
+        d.getTime();
+        System.out.println(1);;
+    }
     
 }
