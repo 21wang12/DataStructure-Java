95a1e45a008b78a4b50adbf3aaa66b6135c13e8a
二一王一二 <sw3310@columbia.edu>
2020-11-05 23:02:38 +0800

    LeetCode-57. 插入区间

--- chapter01/src/collection/Solution.java
+++ chapter01/src/collection/Solution.java
@@ -1,50 +1,34 @@
 package collection;
 import java.util.*;
+
 /**
  * 提交说明：
  * LeetCode-140. 单词拆分 II
  */
 
 class Solution {
-    public List<String> wordBreak(String s, List<String> wordDict) {
-        List<String> l = new ArrayList<>();
-        for(int i = s.length()-1; i >= 0; i++){
-            for(int j = 0; j < s.length(); j++){
-                String word = s.substring(i-j, i+1);
-                    if( wordDict.contains(word)){
-                        i -= j;
-                        l.add(word);
-                    }
-            }
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        int[][] res = new int[intervals.length + 1][2];
+        int idx = 0;
+        // 遍历区间列表：
+        // 首先将新区间左边且相离的区间加入结果集
+        int i = 0;
+        while (i < intervals.length && intervals[i][1] < newInterval[0]) {
+            res[idx++] = intervals[i++];
         }
-        return l;
-    }
-    public int[] intersection(int[] nums1, int[] nums2) {
-        Set<Integer> s1 = new HashSet<>();
-        Set<Integer> s2 = new HashSet<>();
-        for (int i : nums1) {
-            s1.add(i);            
+        // 接着判断当前区间是否与新区间重叠，重叠的话就进行合并，直到遍历到当前区间在新区间的右边且相离，
+        // 将最终合并后的新区间加入结果集
+        while (i < intervals.length && intervals[i][0] <= newInterval[1]) {
+            newInterval[0] = Math.min(intervals[i][0], newInterval[0]);
+            newInterval[1] = Math.max(intervals[i][1], newInterval[1]);
+            i++;
         }
-        for (int i : nums2) {
-            s2.add(i);
+        res[idx++] = newInterval;
+        // 最后将新区间右边且相离的区间加入结果集
+        while (i < intervals.length) {
+            res[idx++] = intervals[i++];
         }
-        s1.retainAll(s2);
-        int[] array = new int[s1.size()];
-        int index = 0;
-        for (int i : s1) {
-            array[index++] = i;
-        }
-        return array;
-    }
 
-    public static void main(String[] args){
-        List<Integer> l = new ArrayList<>();
-        List<Integer> l2 = new ArrayList<>();
-        l.add(2);
-        l.add(2);
-        l2.add(2);
-        l.removeAll(l2);
-        System.out.println("l:"+l);
-
+        return Arrays.copyOf(res, idx);
     }
 }
