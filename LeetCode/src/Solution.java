755bb2972f6ead0fbd8e3e12c7b41b24a28add1c
二一王一二 <sw3310@columbia.edu>
2020-11-17 09:45:03 +0800

    1030. 距离顺序排列矩阵单元格

--- LeetCode/src/Solution.java
+++ LeetCode/src/Solution.java
@@ -5,20 +5,26 @@ import java.util.*;
  */
 
 class Solution {
-    public int[][] reconstructQueue(int[][] people) {
-        Arrays.sort(people,new Comparator<int[]>(){
+    public int[][] allCellsDistOrder(int R, int C, int r0, int c0) {
+        int count = R*C;
+        int[][] ret = new int[count][2];
+        int k = 0;
+        for(int i = 0; i < R; i++){
+            for(int j = 0; j < C; j++){
+                ret[k][0] = i;
+                ret[k][1] = j;
+                k++;
+            }
+        }
+        Arrays.sort(ret,new Comparator<int[]>(){
 
 			@Override
 			public int compare(int[] o1, int[] o2) {
-                // TODO Auto-generated method stub
-				return o1[0]==o2[0]?o1[1]-o2[1]:o2[0]-o1[0];
+				// TODO Auto-generated method stub
+				return Math.abs(o1[0]-r0)+Math.abs(o1[1]-c0)-Math.abs(o2[0]-r0)-Math.abs(o2[1]-c0);
 			}
             
         });
-        List<int[]> l = new ArrayList<>();
-        for (int[] a : people) {
-            l.add(a[1], a);            
-        }
-        return l.toArray(new int[0][]);
+        return ret;
     }
 }
