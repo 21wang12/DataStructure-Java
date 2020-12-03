bd986378dface5a5e1334254a211c3d10bfa0b4a
二一王一二 <sw3310@columbia.edu>
2020-12-03 09:43:19 +0800

    LeetCode-204. 计数质数

--- LeetCode/src/Solution.java
+++ LeetCode/src/Solution.java
@@ -5,23 +5,23 @@ import java.util.*;
  */
 
 class Solution {
-    public int[] searchRange(int[] nums, int target) {
-        int i=0,j=nums.length-1;
-        while(i<=j){
-            if(nums[i]==nums[j]&&nums[i]==target){
+    public int countPrimes(int n) {
+        int cnt = 0;
+        for(int i = 2;i < n;i++){
+            if(isPrime(i)){
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+    public boolean isPrime(int n){
+        boolean result = true;
+        for(int i = 2;i*i<=n;i++){
+            if(n%i==0){
+                result = false;
                 break;
             }
-            if(nums[i]!=target){
-                i++;
-            }
-            if(nums[j]!=target){
-                j--;
-            }
         }
-        if(i<=j){
-            return new int[]{i,j};
-        }
-        return new int[]{-1,-1};
-        
+        return result;
     }
 }
